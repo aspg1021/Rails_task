bfe39ca4497e275e0bada8d1d1728de7f6f6ea84
aspg <atrssk1021@gmail.com>
2025-05-02 14:46:49 +0900

    スケジュール管理アプリの完成

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -24,23 +24,19 @@ class SchedulesController < ApplicationController
   # POST /schedules or /schedules.json
   def create
     @schedule = Schedule.new(schedule_params)
-
-    respond_to do |format|
-      if @schedule.save
-        format.html { redirect_to @schedule, notice: "Schedule was successfully created." }
-        format.json { render :show, status: :created, location: @schedule }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @schedule.errors, status: :unprocessable_entity }
-      end
+    if @schedule.save
+      flash[:notice] = "スケジュールを登録しました"
+      redirect_to schedules_path
+    else
+      render :new
     end
-  end
+  end  
 
   # PATCH/PUT /schedules/1 or /schedules/1.json
   def update
     @schedule = Schedule.find(params[:id])
     if @schedule.update(params.require(:schedule).permit(:title, :start_date, :end_date, :all_day, :memo))
-      flash[:notice] = "ユーザーIDが「#{@schedule.id}」の情報を更新しました"
+      flash[:notice] = "スケジュールを更新しました"
       redirect_to :schedules
     else
       render "edit"
